01f882b048086b03340b9791f0e95ee6ad71af7b
shweta singh <shweta.singh@newelevation.com>
2022-03-22 16:49:36 +0530

    auw Combine executed on production

--- AUW_Combine/src/test/java/Casuality_Prod_TC/Base_class.java
+++ AUW_Combine/src/test/java/Casuality_Prod_TC/Base_class.java
@@ -24,7 +24,7 @@ public class Base_class {
 	@Parameters ("browser")
 	 @BeforeMethod
 	
-/*public void setupApplication()
+public void setupApplication()
 		{
 			
 			Reporter.log("=====FF Browser Session Started=====", true);
@@ -46,7 +46,7 @@ public class Base_class {
 			Reporter.log("=====Application Started=====", true);
 			
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		}*/
+		}
 
 public void setupApplication (String browser)
 		{
